95e340a90abf127f9d5475c53bf361da184ebeb7
Hakkenheiner <remi.zicken@gmail.com>
2024-10-28 15:46:58 +0100

    remi

--- src/App.tsx
+++ src/App.tsx
@@ -8,42 +8,60 @@ import { useState } from "react";
 import Welcome from "./components/Welcome";
 
 function App() {
-	const [page, setPage] = useState ("welcome")
+	const [page, setPage] = useState("welcome");
+
 	const handleClickDev = () => {
-		setPage("dev")
-	}
+		setPage("dev");
+	};
 	const handleClickCda = () => {
-		setPage("cda")
-	}
+		setPage("cda");
+	};
 	const handleClickData = () => {
-		setPage("data")
-	}
+		setPage("data");
+	};
 	const handleClickWelcome = () => {
-		setPage("welcome")
-	}
-  return (
-    <>
-      <Nav />
-      <div className="app-container">
-        <div className="app-button-container">
-		<button onClick={handleClickWelcome} className="app-button" type="button">
-            ACCUEIL
-          </button>
-          <button onClick={handleClickDev} className="app-button" type="button">
-            DEV
-          </button>
-          <button onClick={handleClickData} className="app-button" type="button">
-            DATA
-          </button>
-          <button onClick={handleClickCda} className="app-button" type="button">
-            CDA
-          </button>
-        </div>
-		{page === "dev"? <DevCourse/> : page === "data"? <Data/> : page === "cda"? <Cda/> : <Welcome/>}
-      </div>
-      <Footer />
-    </>
-  );
+		setPage("welcome");
+	};
+
+	return (
+		<>
+			<Nav />
+			<div className="app-container">
+				<div className="app-button-container">
+					<button
+						onClick={handleClickWelcome}
+						className="app-button"
+						type="button"
+					>
+						ACCUEIL
+					</button>
+					<button onClick={handleClickDev} className="app-button" type="button">
+						DEV
+					</button>
+					<button
+						onClick={handleClickData}
+						className="app-button"
+						type="button"
+					>
+						DATA
+					</button>
+					<button onClick={handleClickCda} className="app-button" type="button">
+						CDA
+					</button>
+				</div>
+				{page === "dev" ? (
+					<DevCourse />
+				) : page === "data" ? (
+					<Data />
+				) : page === "cda" ? (
+					<Cda />
+				) : (
+					<Welcome />
+				)}
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
