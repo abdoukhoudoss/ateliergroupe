35b3e569dc82d756d43c1b0839a2a920d9cfc4c7
sylviabarraud2@gmail.com <sylviabarraud2@gmail.com>
2024-10-28 16:58:06 +0100

    ok

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,11 @@
 import "../styles/Welcome.css";
-import "..//styles/Data.css"
+import dev from "../assets/développeur.avif"
 
 function Welcome() {
   const titre = "Welcome à la Wild Code School !";
   const introduction =
     "Pour changer de carrière ou monter en compétences, la Wild Code School propose des formations certifiantes à distance ou sur campus. Développement Web, Data & I.A, Infrastructure & Cybersécurité et Product Design.";
   const slogan = "Vous aurez les codes !";
-  const image = "/src/assets/développeur.avif"
 
   return (
     <>
@@ -14,7 +13,7 @@ function Welcome() {
         <h1 className="titre">{titre}</h1>
         <p className="coucou">{introduction}</p>
         <h3>{slogan}</h3>
-        <img src={image} alt ={titre} className="images"/>
+        <img src={dev} alt ={titre} className="images"/>
       </section>
     </>
   );
